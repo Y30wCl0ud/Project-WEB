3e7a8247425759937785706431bc24b101cb0e1c
Kang Yun Wang <kevinwang8011@hotmail.com>
2016-12-15 19:43:40 +0100

    added a toggle function to the accoutn button which reveals the login form

---
+++ main.js
@@ -0,0 +1,8 @@
+var account = document.getElementById("account_btn");
+
+account.addEventListener("click", toggleLogin, false);
+
+function toggleLogin() {
+  var loginForm = document.getElementById("login_form");
+  loginForm.classList.toggle("visibility");
+}
