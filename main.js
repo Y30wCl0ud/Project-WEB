92f8f75b0046d4eab54c5cb6ab09f1a6384e4224
Kang Yun Wang <kevinwang8011@hotmail.com>
2017-01-17 19:27:40 +0100

    The prototype of card swipe works but has some issues (only the first card. Prototype of filtering works (only red/rood).

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
+// login form
 var account = document.getElementById('account_btn');
-
-account.addEventListener('click', toggleLogin);
 
 function toggleLogin() {
   var loginForm = document.getElementById('login_form');
   loginForm.classList.toggle('visibility');
 }
+
+account.addEventListener('click', toggleLogin);
+
+// mobile filter
+var filterOn = document.querySelector('.filter button');
+var pusher = document.querySelector('.pusher');
+var menu = document.querySelector('body');
+
+
+function mobileFilterToggle(e) {
+  console.log(1);
+  menu.classList.toggle('filter-menu-open');
+}
+
+function closeFilter(e) {
+  if (e.target.classList.contains('pusher')) {
+    menu.classList.toggle('filter-menu-open');
+    console.log(e.target);
+  }
+}
+
+filterOn.addEventListener('click', mobileFilterToggle);
+pusher.addEventListener('click', closeFilter);
--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-var filterOn = document.querySelector('.filter button');
-var pusher = document.querySelector('.pusher');
-var menu = document.querySelector('body');
 
 var check = document.querySelector('#clickmij');
 var item = document.querySelector('.filter-menu li');
 
+var state = false;
+var articles = document.querySelectorAll('.verhalen article');
+var filterItem = document.querySelectorAll('section:nth-of-type(2) li');
 
 
 function scanning(e) {
@@ -13,22 +13,77 @@ function scanning(e) {
     item.classList.toggle('selected');
   }
   console.log(check.checked);
-  console.log(check);
+}
+item.addEventListener('click', filterStories);
+
+for (var i = 0; i < filterItem.length; i++) {
+  filterItem[i].addEventListener('click', filterStories);
+}
+
+function filterStories(e) {
+  var chosenColor = e.target.getAttribute('data-color');
+  console.log(chosenColor);
+  var i = 0;
+  console.log(e.target);
+  //e.target.classList.toggle('selected');
+  //set add class
+  //make a loop check all li for class and remove if they have it
+  if (state === false) {
+
+    for (i = 0; i < articles.length; i++) {
+      if (articles[i].getAttribute('data-color') === chosenColor) {
+        articles[i].classList.remove('filter-order');
+      } else {
+        articles[i].classList.add('filter-order');
+      }
+    }
+    state = true;
+
+  } else {
+    for (i = 0; i < articles.length; i++) {
+      articles[i].classList.remove('filter-order');
+      articles[i].classList.remove('selected');
+    }
+    state = false;
+  }
 
 }
 
-function mobileFilterToggle(e) {
-  console.log(1);
-  menu.classList.toggle('filter-menu-open');
+// console.log(attri[2].getAttribute('data-color'));
+
+
+
+
+// card swipe prototype
+var swipeElements = document.querySelectorAll('.verhalen article section');
+
+for (var i = 0; i < swipeElements.length; i++) {
+  var hammerCard = new Hammer (swipeElements[i]);
+  hammerCard.on('panend', resetElement);
+  i % 2 ? hammerCard.on('panright', swiping) : hammerCard.on('panleft', swiping);
 }
 
-function closeFilter(e) {
-  if (e.target.classList.contains('pusher')) {
-    menu.classList.toggle('filter-menu-open');
-    console.log(e.target);
+
+
+function swiping(event) {
+  var elToSwipe = event.target;
+  // elToSwipe.style.left =  event.deltaX + 'px';
+  elToSwipe.style.transform = 'translateX(' + event.deltaX + 'px)';
+}
+
+
+function resetElement(event) {
+  var toReset = event.target;
+
+  for (var i = 0; i < swipeElements.length; i++) {
+    if (i % 2 == 0 && event.deltaX < -100) {
+      swipeElements[i + 1].style.transform = 'translateX(0px)';
+      swipeElements[i + 1].classList.toggle('trans');
+      swipeElements[i].classList.toggle('trans');
+    } else if (i % 2 == 1 && event.deltaX > 100) {
+      swipeElements[i - 1].style.transform = 'translateX(0px)';
+      swipeElements[i - 1].classList.toggle('trans');
+      swipeElements[i].classList.toggle('trans');
+    }
   }
 }
-
-item.addEventListener('click', scanning);
-filterOn.addEventListener('click', mobileFilterToggle);
-pusher.addEventListener('click', closeFilter);
