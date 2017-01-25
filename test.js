c50075dff6fbed185f5a6bba2c6cf11d26badb02
Kang Yun Wang <kevinwang8011@hotmail.com>
2017-01-25 13:17:27 +0100

    Cleaned js code a bit and added some more annotations

--- test.js
+++ test.js
@@ -1,117 +1,85 @@
 
-var mFilterItem = document.querySelectorAll('.filter-menu li');
+var mFilterTile = document.querySelectorAll('.filter-menu li'),
+    filterTile = document.querySelectorAll('section:nth-of-type(2) li'),
+    filterInfo = document.querySelectorAll('section:nth-of-type(2) li p')
 
-var articles = document.querySelectorAll('.verhalen article');
-var filterItem = document.querySelectorAll('section:nth-of-type(2) li');
-var filterInfo = document.querySelectorAll('section:nth-of-type(2) li p')
-var windowSize;
+var mFilterTrigger = document.querySelector('section:nth-of-type(2) ul'),
+    pusher = document.querySelector('main'),
+    pageBody = document.querySelector('body');
 
-var stories = document.querySelector('section:nth-of-type(3)');
+var articles = document.querySelectorAll('.verhalen article'),
+    stories = document.querySelector('section:nth-of-type(3)'),
+    chosenColor,
+    storiesOffset,
+    pageOffset;
 
 
-var pusher = document.querySelector('main');
-var pageBody = document.querySelector('body');
 
 
-function mobileFilterToggle(e) {
+// two functions to toggle the mobile color filter
+function mobileFilterToggle() {
   if (window.innerWidth < 480) {
     pageBody.classList.toggle('filter-menu-open');
   }
 }
 
-function closeFilter(e) {
-  if (e.target.getAttribute('data-effect') === 'pusher') {
+function closeFilter(event) {
+  if (event.target.getAttribute('data-effect') === 'pusher') {
     pageBody.classList.toggle('filter-menu-open');
-    console.log(e.target);
   }
 }
 
-pusher.addEventListener('click', closeFilter);
-
-
-
-var storiesOffset;
-var pageOffset;
-
-function filterStories(e) {
+// function to select the correct filter tile, filter the stories & scroll to results
+function filterStories(event) {
   storiesOffset = stories.offsetTop;
   pageOffset = window.pageYOffset;
-  // var foo = true;
 
+  chosenColor = event.target.getAttribute('data-color');
 
-
-
-  var chosenColor = e.target.getAttribute('data-color');
-
-// filtering the articles
+  // filtering the articles
   for (var i = 0; i < articles.length; i++) {
     articles[i].classList.add('filtered');
-
     if (articles[i].getAttribute('data-color') === chosenColor) {
       articles[i].classList.remove('filtered');
-    } else {
     }
   }
 
-  // check whether the filter is selected or not
-    if (e.target.classList.contains('selected')) {
-      e.target.classList.remove('selected');
-    // to deactivate a already active filter
-      for (var i = 0; i < articles.length; i++) {
-        articles[i].classList.remove('filtered');
-      }
-    } else {
-      for (var i = 0; i < mFilterItem.length; i++) {
-        filterItem[i].classList.remove('selected');
-        mFilterItem[i].classList.remove('selected');
-      }
-      pageBody.classList.add('transition');
-      if (storiesOffset != pageOffset) {
-        pageBody.style.transform = "translate(0, -" + (storiesOffset - pageOffset) + "px)";
-      }
+  // check whether the filter (tile) is selected or not
+  if (event.target.classList.contains('selected')) {
+    event.target.classList.remove('selected');
+  // to show all the artivles again
+    for (var i = 0; i < articles.length; i++) {
+      articles[i].classList.remove('filtered');
+    }
+  } else {
+    for (var i = 0; i < mFilterTile.length; i++) {
+      filterTile[i+1].classList.remove('selected');
+      mFilterTile[i].classList.remove('selected');
+    }
+    event.target.classList.add('selected');
 
-      window.setTimeout(function(){
-        pageBody.classList.remove('transition');
-        pageBody.style.cssText = "";
-          window.scrollTo(0, storiesOffset);
-      }, 900)
-
-      e.target.classList.add('selected');
+    // the scroll transition to the stories/articles
+    pageBody.classList.add('transition');
+    if (storiesOffset != pageOffset) {
+      pageBody.style.transform = "translate(0, -" + (storiesOffset - pageOffset) + "px)";
     }
 
-
-}
-
-// console.log(attri[2].getAttribute('data-color'));
-window.addEventListener('resize', function() {
-  // console.log(window.innerWidth);
-  windowSize = window.innerWidth;
-});
-
-
-for (var i = 1; i < filterItem.length; i++) {
-  filterItem[i].addEventListener('click', filterStories);
-  // filterItem[i].addEventListener('mouseover', showInfo);
-
-}
-
-for (var i = 0; i < mFilterItem.length; i++) {
-  mFilterItem[i].addEventListener('click', filterStories);
+    window.setTimeout(function() {
+      pageBody.classList.remove('transition');
+      pageBody.style.cssText = "";
+        window.scrollTo(0, storiesOffset);
+    }, 900)
+  }
 }
 
 
+mFilterTrigger.addEventListener('click', mobileFilterToggle);
+pusher.addEventListener('click', closeFilter);
 
-
-
-function showInfo(e) {
-  // var target = document.querySelector(e);
-  var target = e.target.childnodes;
-  console.log(target);
-  // articles[1].scrollIntoView(true);
+for (var i = 1; i < filterTile.length; i++) {
+  filterTile[i].addEventListener('click', filterStories);
 }
 
-
-
-var mobiletrigger = document.querySelector('section:nth-of-type(2) ul');
-
-mobiletrigger.addEventListener('click', mobileFilterToggle);
+for (var i = 0; i < mFilterTile.length; i++) {
+  mFilterTile[i].addEventListener('click', filterStories);
+}
