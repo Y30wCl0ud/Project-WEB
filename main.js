ee8d6ca334f994e9de44fac821c1a1b24c07424e
Kang Yun Wang <kevinwang8011@hotmail.com>
2017-01-26 16:12:00 +0100

    Added a minimal transition on the loginform input where it animates the border color. Can select the corresponding child element now by interacting with the parent element (for the color infor by hovering). Added all the info about the color in the tile(desktop). Styling it now and animating.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-// login form
+/*=============================================>>>>>
+= loginform =
+===============================================>>>>>*/
 var account = document.getElementById('account_btn');
 
 function toggleLogin() {
@@ -8,45 +10,20 @@ function toggleLogin() {
 
 account.addEventListener('click', toggleLogin);
 
-// mobile filter
-// var filterOn = document.querySelector('.filter button');
-// var pusher = document.querySelector('.pusher');
-// var menu = document.querySelector('body');
-//
-//
-// function mobileFilterToggle(e) {
-//   console.log(1);
-//   menu.classList.toggle('filter-menu-open');
-// }
-//
-// function closeFilter(e) {
-//   if (e.target.classList.contains('pusher')) {
-//     menu.classList.toggle('filter-menu-open');
-//     console.log(e.target);
-//   }
-// }
-//
-// filterOn.addEventListener('click', mobileFilterToggle);
-// pusher.addEventListener('click', closeFilter);
-
-
 /*=============================================>>>>>
 = like and bookmark events =
 ===============================================>>>>>*/
 
-
 var articleLike = document.getElementById('like');
 var articleBookmark = document.getElementById('bookmark');
 
-function aniArtiSelection(e) {
-  e.target.classList.toggle('animated');
-  e.target.classList.toggle('inactiveArtiItem');
+function aniArtiSelection(event) {
+  event.target.classList.toggle('animated');
+  event.target.classList.toggle('inactiveArtiItem');
 }
 
 articleLike.addEventListener('click', aniArtiSelection);
 articleBookmark.addEventListener('click', aniArtiSelection);
-
-
 
 
 /*=============================================>>>>>
@@ -87,4 +64,91 @@ function resetElement(event) {
       }
     }
   }
+}
+
+/*=============================================>>>>>
+= filtersystem =
+===============================================>>>>>*/
+var mFilterTile = document.querySelectorAll('.filter-menu li'),
+    filterTile = document.querySelectorAll('section:nth-of-type(2) li');
+
+var mFilterTrigger = document.querySelector('section:nth-of-type(2) ul'),
+    pusher = document.querySelector('main'),
+    pageBody = document.querySelector('body');
+
+var articles = document.querySelectorAll('.verhalen article'),
+    stories = document.querySelector('section:nth-of-type(3)'),
+    chosenColor,
+    storiesOffset,
+    pageOffset;
+
+
+
+
+// two functions to toggle the mobile color filter
+function mobileFilterToggle() {
+  if (window.innerWidth < 480) {
+    pageBody.classList.toggle('filter-menu-open');
+  }
+}
+
+function closeFilter(event) {
+  if (event.target.getAttribute('data-effect') === 'pusher') {
+    pageBody.classList.toggle('filter-menu-open');
+  }
+}
+
+// function to select the correct filter tile, filter the stories & scroll to results
+function filterStories(event) {
+  storiesOffset = stories.offsetTop;
+  pageOffset = window.pageYOffset;
+
+  chosenColor = event.target.getAttribute('data-color');
+
+  // filtering the articles
+  for (var i = 0; i < articles.length; i++) {
+    articles[i].classList.add('filtered');
+    if (articles[i].getAttribute('data-color') === chosenColor) {
+      articles[i].classList.remove('filtered');
+    }
+  }
+
+  // check whether the filter (tile) is selected or not
+  if (event.target.classList.contains('selected')) {
+    event.target.classList.remove('selected');
+  // to show all the artivles again
+    for (var i = 0; i < articles.length; i++) {
+      articles[i].classList.remove('filtered');
+    }
+  } else {
+    for (var i = 0; i < mFilterTile.length; i++) {
+      filterTile[i+1].classList.remove('selected');
+      mFilterTile[i].classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+
+    // the scroll transition to the stories/articles
+    pageBody.classList.add('transition');
+    if (storiesOffset != pageOffset) {
+      pageBody.style.transform = "translate(0, -" + (storiesOffset - pageOffset) + "px)";
+    }
+
+    window.setTimeout(function() {
+      pageBody.classList.remove('transition');
+      pageBody.style.cssText = "";
+        window.scrollTo(0, storiesOffset);
+    }, 900)
+  }
+}
+
+
+mFilterTrigger.addEventListener('click', mobileFilterToggle);
+pusher.addEventListener('click', closeFilter);
+
+for (var i = 1; i < filterTile.length; i++) {
+  filterTile[i].addEventListener('click', filterStories);
+}
+
+for (var i = 0; i < mFilterTile.length; i++) {
+  mFilterTile[i].addEventListener('click', filterStories);
 }
--- test.js
+++ test.js
@@ -1,85 +1,12 @@
+var filterTile = document.querySelectorAll('section:nth-of-type(2) li');
 
-var mFilterTile = document.querySelectorAll('.filter-menu li'),
-    filterTile = document.querySelectorAll('section:nth-of-type(2) li'),
-    filterInfo = document.querySelectorAll('section:nth-of-type(2) li p')
-
-var mFilterTrigger = document.querySelector('section:nth-of-type(2) ul'),
-    pusher = document.querySelector('main'),
-    pageBody = document.querySelector('body');
-
-var articles = document.querySelectorAll('.verhalen article'),
-    stories = document.querySelector('section:nth-of-type(3)'),
-    chosenColor,
-    storiesOffset,
-    pageOffset;
-
-
-
-
-// two functions to toggle the mobile color filter
-function mobileFilterToggle() {
-  if (window.innerWidth < 480) {
-    pageBody.classList.toggle('filter-menu-open');
-  }
-}
-
-function closeFilter(event) {
-  if (event.target.getAttribute('data-effect') === 'pusher') {
-    pageBody.classList.toggle('filter-menu-open');
-  }
-}
-
-// function to select the correct filter tile, filter the stories & scroll to results
-function filterStories(event) {
-  storiesOffset = stories.offsetTop;
-  pageOffset = window.pageYOffset;
-
-  chosenColor = event.target.getAttribute('data-color');
-
-  // filtering the articles
-  for (var i = 0; i < articles.length; i++) {
-    articles[i].classList.add('filtered');
-    if (articles[i].getAttribute('data-color') === chosenColor) {
-      articles[i].classList.remove('filtered');
-    }
-  }
-
-  // check whether the filter (tile) is selected or not
-  if (event.target.classList.contains('selected')) {
-    event.target.classList.remove('selected');
-  // to show all the artivles again
-    for (var i = 0; i < articles.length; i++) {
-      articles[i].classList.remove('filtered');
-    }
-  } else {
-    for (var i = 0; i < mFilterTile.length; i++) {
-      filterTile[i+1].classList.remove('selected');
-      mFilterTile[i].classList.remove('selected');
-    }
-    event.target.classList.add('selected');
-
-    // the scroll transition to the stories/articles
-    pageBody.classList.add('transition');
-    if (storiesOffset != pageOffset) {
-      pageBody.style.transform = "translate(0, -" + (storiesOffset - pageOffset) + "px)";
-    }
-
-    window.setTimeout(function() {
-      pageBody.classList.remove('transition');
-      pageBody.style.cssText = "";
-        window.scrollTo(0, storiesOffset);
-    }, 900)
-  }
-}
-
-
-mFilterTrigger.addEventListener('click', mobileFilterToggle);
-pusher.addEventListener('click', closeFilter);
 
 for (var i = 1; i < filterTile.length; i++) {
-  filterTile[i].addEventListener('click', filterStories);
+  filterTile[i].addEventListener('mouseenter', showInfo);
+  filterTile[i].addEventListener('mouseout', showInfo);
 }
 
-for (var i = 0; i < mFilterTile.length; i++) {
-  mFilterTile[i].addEventListener('click', filterStories);
+function showInfo(event) {
+  var hoverOn = this.children[1];
+  hoverOn.classList.toggle('visibility');
 }
